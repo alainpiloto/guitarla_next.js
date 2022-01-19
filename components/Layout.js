4eb450947e330dcfe8f1d02e0457246293da0429
alainpiloto <alainpiloto@gmail.com>
2022-01-19 01:52:03 -0500

    package json modified

--- components/Layout.js
+++ components/Layout.js
@@ -10,7 +10,7 @@ const Layout = ({children, pagina, guitarra}) => {
                 <meta name="description" content="Sitio Web de Venta de Guitarras" />
                 
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;700;900&family=Roboto:wght@300;400;700;900&family=Staatliches&display=swap" rel="stylesheet"/>
             </Head>
             <Header 
